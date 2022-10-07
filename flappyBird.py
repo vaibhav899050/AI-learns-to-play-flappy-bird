2b49dda90fb023b9833c81e31f98c695fcb44ecc
Vaibhav <vg899050@gmail.com>
2022-10-07 22:23:55 +0530

    initial commit

---
+++ flappyBird.py
@@ -0,0 +1,45 @@
+import time
+import neat
+import pygame
+import os
+import random
+
+
+WIN_WIDTH = 800
+WIN_HEIGHT = 600
+
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load((os.path.join("imgs", "bird1.png")))), pygame.transform.scale2x(pygame.image.load((os.path.join("imgs", "bird2.png")))), pygame.transform.scale2x(pygame.image.load((os.path.join("imgs", "bird3.png"))))]
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load((os.path.join("imgs", "pipe.png"))))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load((os.path.join("imgs", "base.png"))))
+BG_IMG = pygame.transform.scale2x(pygame.image.load((os.path.join("imgs", "bg.png"))))
+
+
+class bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25
+    ROT_VEL = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.height = y
+        self.vel = 0
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count = 1
+
+while True:
+    bird.move()
+
